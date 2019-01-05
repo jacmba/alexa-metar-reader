9d63f835656850761c68787c52be021e364acec5
Jacinto Mba Cantero <jacmba@gmail.com>
2019-01-05 22:27:51 +0000

    Fixed models. Added test cases for letter parsing (es-ES)

--- lambda/custom/test/unit/metarTests.js
+++ lambda/custom/test/unit/metarTests.js
@@ -7,6 +7,11 @@ suite('Metar business logic unit tests', () => {
 
   test('ES language letter identification tests', () => {
     const a = metar.getLetter('es', 'alfa')
+    const m = metar.getLetter('es', 'maik')
+    const y = metar.getLetter('es', 'yanki')
+
     assert.strictEqual(a, 'A', 'alfa should be A')
+    assert.strictEqual(m, 'M', 'maik should be M')
+    assert.strictEqual(y, 'Y', 'yanki should be Y')
   })
 })
