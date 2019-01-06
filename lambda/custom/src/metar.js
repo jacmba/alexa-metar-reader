4048f48f642f4cbc19f98c34008f3a74eb0ad187
Jacinto Mba Cantero <jacmba@gmail.com>
2019-01-06 01:59:40 +0000

    basic working version

--- lambda/custom/src/metar.js
+++ lambda/custom/src/metar.js
@@ -1,13 +1,38 @@
 'use strict'
 
+const request = require('request-promise')
+
+const AIRPORTS_ADDRESS = 'http://airports.api.jazbelt.net'
+const METAR_ADDRESS = 'http://metar.api.jazbelt.net'
+
+const buildOpt = uri => ({
+  uri: uri,
+  json: true
+})
+
 const getLetter = (language, phonLetter) => {
   const dictFile = `../dictionaries/${language}.json`
   const dictionary = require(dictFile)
   return dictionary[phonLetter]
 }
 
-const getMetar = icao => {
-  return 'Metar ' + icao
+const getAirport = async icao => {
+  const uri = AIRPORTS_ADDRESS + '/airport/' + icao
+  const opt = buildOpt(uri)
+
+  const airport = await request(opt)
+  return airport[0].name
 }
 
-module.exports = {getLetter, getMetar}
+const getMetar = async icao => {
+  const uri = METAR_ADDRESS + '/metar/' + icao
+  const opt = buildOpt(uri)
+
+  const metar = await request(opt)
+  return metar
+}
+
+const parseMetar = metar =>
+  metar.split('').join(' ')
+
+module.exports = {getLetter, getAirport, getMetar, parseMetar}
